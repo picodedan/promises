7f710f0664248b76ec1b363b9257ee39af780df7
Hack Reactor Student <student@hackreactor.com>
2017-07-27 20:44:23 -0700

    callback review completed, on to promis constructor

--- exercises/bare_minimum/callbackReview.js
+++ exercises/bare_minimum/callbackReview.js
@@ -6,13 +6,31 @@ var fs = require('fs');
 var request = require('request');
 
 // This function should retrieve the first line of the file at `filePath`
-var pluckFirstLineFromFile = function (filePath) {
-  // TODO
+var pluckFirstLineFromFile = function (filePath, callback) {
+  //access filePath
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      return callback(err);
+    }
+      //isolate first line
+    var linesArr = data.split('\n');
+    //return callback(first line)
+    return callback(err, linesArr[0]);
+  });
 };
 
 // This function should retrieve the status code of a GET request to `url`
-var getStatusCode = function (url) {
-  // TODO
+var getStatusCode = function (url, callback) {
+  var options = {
+    url: url,
+  };
+  request.get(options, function(err, res, body) {
+    if (err) {
+      return callback(err);
+    }
+    return callback(err, res.statusCode);
+  });
+  
 };
 
 // Export these functions so we can test them and reuse them in later exercises
